783da55203bcd5596f2e1990b62f075ebdb3229e
Emilia Huerta <39370518+Icetalon21@users.noreply.github.com>
2019-10-24 00:21:27 -0700

    added a loop so it plays twice

--- Example1.rb
+++ Example1.rb
@@ -1,9 +1,11 @@
 # Welcome to Sonic Pi v3.1
-play 60
-sleep 1
-play 65
-sleep 1
-play 70
-sleep 1
-play 75
-sleep 1
+2.times do
+  play 60
+  sleep 1
+  play 65
+  sleep 1
+  play 70
+  sleep 1
+  play 75
+  sleep 1
+end
